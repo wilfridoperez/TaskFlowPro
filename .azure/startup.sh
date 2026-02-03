7fd2229dab085e06930820b62ba66a673a002f44
Your Namewilfridoperez@hotmail.comwilfridoperez@ <wilfridoperez@hotmail.com>
2026-02-03 17:27:16 -0500

    Use npm start for deployment

--- .azure/startup.sh
+++ .azure/startup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Startup script for Azure App Service
-# Start the Next.js app (build should have already happened during deployment)
+# Start the Next.js app
 
-# Dynamically set NEXTAUTH_URL based on the app's actual URL
+# Set NEXTAUTH_URL
 if [ -z "$NEXTAUTH_URL" ]; then
     if [ ! -z "$WEBSITE_HOSTNAME" ]; then
         export NEXTAUTH_URL="https://$WEBSITE_HOSTNAME"
@@ -12,6 +12,5 @@ fi
 
 cd /home/site/wwwroot
 
-# Start Next.js server
-# Use exec to replace the shell process
-exec node_modules/.bin/next start --port ${PORT:-8080}
+# Try to start with npm start, fallback to next start
+npm start
