7e48580827de6f1260e4de08b2a05a01fb2f8d66
Your Namewilfridoperez@hotmail.comwilfridoperez@ <wilfridoperez@hotmail.com>
2026-02-04 06:57:17 -0500

    Fix Azure deployment: ensure Prisma client generated, simplify postinstall

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-#!/usr/bin/env node
-// Next.js startup wrapper
-require('next/dist/bin/next').main(['start']);
+// Azure App Service uses npm start from package.json
+// Next.js server is started with: next start --port ${PORT:-8080}
+// This file is not needed but kept for reference
 
